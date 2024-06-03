6350b16006aa9049d114fb5758c7c1fb9684d3ca
Josh <164903120+Joshua-Gross@users.noreply.github.com>
2024-06-03 14:51:37 +0100

    corrected capitalisation in ItemList.jsx

--- public/react/components/ItemList.jsx
+++ public/react/components/ItemList.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Item } from './Item'; 
 
-export const ItemList = ({Items}) => {
+export const ItemList = ({items}) => {
 	return <>
 		{
-			Items.map((item, idx) => {
+			items.map((item, idx) => {
 				return <Item Item={item} key={idx} />
 			})
 		}
