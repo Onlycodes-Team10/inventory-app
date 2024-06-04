f186f8cfe0e8407c23ef59e8955115fb2d7209a2
Bushra Fatima <bushrafatima@192.168.1.111>
2024-06-04 10:46:52 +0100

    created delete route for deleting one item

--- server/routes/items.js
+++ server/routes/items.js
@@ -22,4 +22,19 @@ router.get("/:id", async (req, res, next) => {
   }
 });
 
+// DELETE /items/:id 
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const oneItem = await Items.findByPk(req.params.id);
+    await oneItem.destroy()
+    res.send("Item deleted successfully");
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+
+
+
 module.exports = router;
