d2325e33976ec5e787ab9eadcbfdb3fc76d86a4e
Bushra Fatima <bushrafatima@192.168.1.111>
2024-06-04 10:59:46 +0100

    tests for deleting one item by id

--- server/app.test.js
+++ server/app.test.js
@@ -9,4 +9,10 @@ test("can get one item by id", async () => {
     expect(data).toEqual(expect.objectContaining(items[0]))
 })
 
+test("delete item by id", async ()=>{
+    const res = await request(app).delete("/api/items/1")
+    expect(res.statusCode).toBe(200)
+    const data = res.text
+    expect(data).toEqual("Item deleted successfully")
+})
 
