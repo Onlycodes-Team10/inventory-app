4af5dc4067697ad6b1d12869c22e5d25ac144eba
Addison Rogers <addisonrogers@fsp.co>
2024-06-05 11:18:57 +0100

    stash

--- server/app.test.js
+++ server/app.test.js
@@ -1,13 +1,13 @@
-//const request = require("supertest");
-//const app = require("./app.js")
-//const {items} = require("./seedData.js")
+const request = require("supertest");
+const app = require("./app.js");
+const { items } = require("./seedData.js");
 
-//test("can get one item by id", async () => {
-//    const res = await request(app).get("/api/items/1")
-//    expect(res.statusCode).toBe(200)
-//    const data = JSON.parse(res.text)
-//    expect(data).toEqual(expect.objectContaining(items[0]))
-//})
+test("can get one item by id", async () => {
+    const res = await request(app).get("/api/items/1");
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toEqual(expect.objectContaining(items[0]));
+});
 
 //test("delete item by id", async ()=>{
 //    const res = await request(app).delete("/api/items/1")
@@ -15,4 +15,3 @@
 //    const data = res.text
 //    expect(data).toEqual("Item deleted successfully")
 //})
-
